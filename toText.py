b9b7f1c855e337b5a7999d4c848b706667010013
wmorgus <31784710+wmorgus@users.noreply.github.com>
2017-09-09 15:41:39 -0400

    updated tweets text, finished toText script

--- toText.py
+++ toText.py
@@ -1,1 +1,39 @@
-#This is a comment
+import json
+
+def makeItPretty(current):
+	new = current
+	start = current.find('https://t.co/')
+	if (start != -1):
+		end = start + 24
+		new = current[0:start] + current[end:]
+	return new
+
+
+data_file = open('/Users/willmorgus/Desktop/trumpitter/tweets.json', 'r')  
+data = data_file.read()
+data_file.close()
+
+x = json.loads(data)
+
+
+new = 'https://t.co/twIgAKJs3s'
+two = new
+start = new.find('https://t.co/')
+if (start != -1):
+	end = start + 24
+	two = new[0:start] + new[end:]
+print(two)
+
+o = open('/Users/willmorgus/Desktop/trumpitter/tweetText.txt', 'w')
+for obj in x:
+	new = obj['content']
+	two = new
+	start = new.find('https://t.co/')
+	if (start != -1):
+		end = start + 24
+		two = new[0:start] + new[end:]
+	if (two.find('https://t.co/') == -1):
+		o.write(two.encode('UTF-8') + '\n')
+	
+
+
