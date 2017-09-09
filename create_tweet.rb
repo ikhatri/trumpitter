23ca0489532adceb6d66b6fb79991fe5c56334e2
Jasmine Lee <leejas2017@gmail.com>
2017-09-09 14:42:15 -0400

    finally figured out reading a file into a string

--- create_tweet.rb
+++ create_tweet.rb
@@ -1,2 +1,15 @@
+#Reading from the tweetText.txt
+
+test_string = File.open("test_text.txt") {|text| text.read}
+
+# test_string = "wtf"
+# File.open("test_text.txt").each do |line|
+#	test_string + line
+#end
+
+puts test_string
+
+dictionary = {}
+
 #Choose random word from the list of next possible words
 
