d50f5edf41a3aba9029892b4e0bcff5911004df2
Jasmine Lee <leejas2017@gmail.com>
2017-09-09 15:34:07 -0400

    tested and fixed find_next_word

--- create_tweet.rb
+++ create_tweet.rb
@@ -1,11 +1,6 @@
 #Reading from the tweetText.txt
 test_string = File.open("test_text.txt") {|t| t.read}
-puts test_string #for testing
-
-#puts "w".eql? "w" #testing the equals method (it works!!)
-
-#substring_test = "does this work" #figuring out substrings in ruby
-#puts substring_test[1..-1]
+#puts test_string #for testing
 
 #Identifying different words in string
 dictionary = {}
@@ -18,9 +13,11 @@ def find_next_word(text)
 	if(char.eql? " ")
 		""
 	else
-		char + find_next_word(text[1, -1])
+		char + find_next_word(text[1..-1])
 	end
 end
+
+puts find_next_word(test_string)
 
 #Choose random word from the list of next possible words
 
